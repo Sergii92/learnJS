1d5abcd8a5ab5baf8c78f887022454477fba1383
Sergii <sergii.mykhailyk@gmail.com>
2020-04-14 22:59:06 +0300

    ДЗ1

---
+++ main.js
@@ -0,0 +1,349 @@
+// let hello = "hello";
+// console.log("1", hello);
+// document.write(hello);
+// hello = "ololo";
+// console.log("1", hello);
+// document.write(hello);
+
+// let owu = "owu";
+// console.log("2", owu);
+// document.write(owu);
+
+// owu = "owu111";
+// console.log("2", owu);
+// document.write(owu);
+
+// let com = "com";
+// console.log("3", com);
+// document.write(com);
+
+// com = "com2020";
+// console.log("3", com);
+// document.write(com);
+
+// let ua = "ua";
+// console.log("4", ua);
+// document.write(ua);
+// ua = "ua20";
+// console.log("4", ua);
+// document.write(ua);
+
+// let number1 = 1;
+// console.log("5", number1);
+// document.write(number1);
+
+// number1 = 2;
+// console.log("5", number1);
+// document.write(number1);
+
+// let number2 = 10;
+// console.log("6", number2);
+// document.write(number2);
+
+// number2 = 13;
+// console.log("6", number2);
+// document.write(number2);
+
+// let number3 = -999;
+// console.log("7", number3);
+// document.write(number3);
+
+// number3 = 1999;
+// console.log("7", number3);
+// document.write(number3);
+
+// let number4 = 123;
+// console.log("8", number4);
+// document.write(number4);
+
+// number4 = 1230000;
+// console.log("8", number4);
+// document.write(number4);
+
+// let numberPI = 3.14;
+// console.log("9", numberPI);
+// document.write(numberPI);
+
+// numberPI = 3.14159265;
+// console.log("9", numberPI);
+// document.write(numberPI);
+
+// let number5 = 2.7;
+// console.log("10", number5);
+// document.write(number5);
+
+// number5 = 2.711111;
+// console.log("10", number5);
+// document.write(number5);
+
+// let number6 = 16;
+// console.log("11", number6);
+// document.write(number6);
+
+// number6 = 32;
+// console.log("11", number6);
+// document.write(number6);
+
+// let booleanTrue = true;
+// console.log("12", booleanTrue);
+// document.write(booleanTrue);
+
+// booleanTrue = false;
+// console.log("12", booleanTrue);
+// document.write(booleanTrue);
+
+// let booleanFalse = false;
+// console.log("13", booleanFalse);
+// document.write(booleanFalse);
+
+// booleanFalse = true;
+// console.log("13", booleanFalse);
+// document.write(booleanFalse);
+
+// const a = 12;
+// const b = 13;
+// const c = 17;
+// const str1 = "hi";
+// const str2 = "hello";
+// const str3 = "by";
+// console.log(a, b, c, str1, str2, str3);
+
+// let name = prompt("введите свое имя");
+
+// let lastName = prompt("введите свою фамилию");
+// let fatherName = prompt("введите свое отчество");
+
+// let person = `${lastName} ${name} ${fatherName}`;
+// console.log(`Мое ФИО ${lastName} ${name} ${fatherName}`);
+
+// let motherName = prompt("введите имя мамы");
+
+// let motherLastName = prompt("введите  фамилию мамы");
+// let motherFatherName = prompt("введите отчество мамы");
+
+// let mom = `${motherLastName} ${motherName} ${motherFatherName}`;
+// console.log(
+//   `Моя мама ${motherLastName} ${motherName} ${motherFatherName}`
+// );
+
+// console.log(person);
+// let a = +prompt("num 1=");
+// let b = +prompt("num 2=");
+// let c = +prompt("num 3=");
+// console.log(a, b, c);
+
+// let a = parseInt(prompt("num 1="));
+// let b = parseInt(prompt("num 2="));
+// let c = parseInt(prompt("num 3="));
+// let d = parseInt(prompt("num 4="));
+// let result = a + b + c + d;
+// console.log(result);
+// let a = parseFloat(prompt("num 1="));
+// let b = parseFloat(prompt("num 2="));
+// let c = parseFloat(prompt("num 3="));
+// let d = parseFloat(prompt("num 4="));
+// let result = a + b + c + d;
+// console.log(result);
+
+// let a = Math.round(prompt("num 1="));
+// let b = Math.round(prompt("num 2="));
+// let c = Math.round(prompt("num 3="));
+// let result = a + b + c;
+// console.log(result);
+
+// let a = parseInt(prompt("num 1="));
+// let b = parseInt(prompt("num 2="));
+// let result = Math.pow(a, b);
+// console.log(result);
+
+// let a = 100;
+// let b = "100";
+// let c = true;
+// let d = undefined;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// let a = 5 < 6;
+// let b = 5 > 6;
+// let c = 5 === 6;
+// let d = 5 >= 6;
+// let e = 10 === 10;
+// let f = 10 <= 10;
+// let g = 10 != 10;
+// let i = 10 < 10;
+// let h = 10 > 10;
+// let m = 123 === "123";
+// let n = 123 == "123";
+
+/********************* 
+ console.log(132 > 100 && 45 < 12 );-false
+console.log(34 > 33 && 23 < 90 );-true
+console.log(99 > 100 && 45 > 12 );-false
+console.log(132 > 100 || 45 < 12 );-true
+console.log(111 > 11 || 45 < 111 );-true
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );-true
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );-true
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );-false
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));-true
+console.log(!!'-1');-true
+console.log(!!-1);-true
+console.log(!!'0');-false
+console.log(!!'null');-true
+console.log(!!'undefined');-true
+console.log(!!(3/'owu'));-false
+console.log((111 > 11 || 45 < 111) ||  !!'0');
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));-false
+ *********************/
+
+// let str = "Привет";
+// let num = 123;
+// let flag = true;
+// let txt = "true";
+// console.log("str- ", typeof str);
+// console.log("num- ", typeof num);
+// console.log("flag- ", typeof flag);
+// console.log("txt- ", typeof txt);
+
+// let a1 = 5 + 3;
+// let a2 = 5 - 3;
+// let a3 = 5 * 3;
+// let a4 = 5 / 3;
+// let a5 = 5 % 3;
+// console.log("a1=", a1);
+// console.log("a2=", a2);
+// console.log("a3=", a3);
+// console.log("a4=", a4);
+// console.log("a5=", a5);
+
+// let a6 = 5 % 3;
+// let a7 = 3 % 5;
+// let a8 = 5 + "3";
+// let a9 = "5" - 3;
+// let a10 = 75 + "кг";
+// console.log("a6=", a6);
+// console.log("a7=", a7);
+// console.log("a8=", a8);
+// console.log("a9=", a9);
+// console.log("a10=", a10);
+
+let height = "23см";
+let width = "10см";
+let s = parseInt(height) * parseInt(width);
+console.log(s);
+
+let heightC = 10;
+let dC = 4;
+let v = Math.PI * dC ** 2 * heightC;
+console.log(v);
+
+let n = 3;
+let m = 4;
+let k = Math.sqrt(n ** 2 + m ** 2);
+console.log(k);
+
+// let name = "Sergii";
+// let lastName = "Mykhailyk";
+// let fatherName = "Valeriyovych";
+// alert(`${lastName} \n${name}\n${fatherName}`);
+
+let str1 = "Кто ";
+let str2 = "ты ";
+let str3 = "такой?";
+let concatenation = str1 + str2 + str3;
+console.log(concatenation);
+
+let str = "20";
+let a = 5;
+document.write(str + a + "<br/>");
+//  - 205(строка + сисло= строка)
+document.write(str - a + "<br/>");
+// 15(str станет  числом при вычитании )
+document.write(str * "2" + "<br/>");
+// 40 при умножении str будет числом
+document.write(str / 2 + "<br/>");
+// 10 при делении str будет числом
+
+/******* 
+ parseInt("3.14") -3
+parseInt("-7.875") - (-7)
+parseInt("435")- 435
+parseInt("Вася")- NaN
+ * 
+*/
+// let str = prompt("Enter something", "ho-ho");
+// console.log(str);
+
+// let num1 = +prompt("введите первое число");
+// let num2 = +prompt("введите второе число");
+// let sum = num1 + num2;
+// alert(sum);
+
+// let name = prompt("Ваше имя");
+// let lastName = prompt("Ваша фамилия");
+// let age = prompt("Ваш возраст");
+
+// let message = `Доброго вечера ${lastName} ${name}, мои поздравления что вам ${age}`;
+// alert(message);
+
+// let a1 = +prompt("num1");
+// let a2 = +prompt("num2");
+// let a3 = +prompt("num3");
+// if (a1 > a2 && a1 > a3 && a2 > a3) {
+//   console.log(a3, a2, a1);
+// } else if (a1 > a2 && a1 > a3 && a2 < a3) {
+//   console.log(a2, a3, a1);
+// } else if (a2 > a1 && a2 > a3 && a1 > a3) {
+//   console.log(a3, a1, a2);
+// } else if (a2 > a1 && a2 > a3 && a1 < a3) {
+//   console.log(a1, a3, a2);
+// } else if (a3 > a1 && a3 > a2 && a1 < a2) {
+//   console.log(a1, a2, a3);
+// } else if (a3 > a1 && a3 > a2 && a1 > a2) {
+//   console.log(a2, a1, a3);
+// }
+
+// let color = prompt("Какой цвет световора");
+// let go = "";
+// if (color == "зеленый") {
+//   go = "иди";
+// } else if (color == "желтый") {
+//   go = "подожди";
+// } else if (color == "красный") {
+//   go = "стой";
+// } else {
+//   go = "делай что хочешь!";
+// }
+// switch (color) {
+//   case "зеленый":
+//     console.log("иди");
+//     break;
+//   case "желтый":
+//     console.log("подожди");
+//     break;
+//   case "красный":
+//     console.log("стой");
+//     break;
+//   default:
+//     console.log("делай что хочешь");
+// }
+let color = prompt("Какой цвет световора");
+let isRoadClear = confirm("На дороге есть машыны?");
+let go = "";
+if (color == "зеленый" && !isRoadClear) {
+  go = "иди";
+} else if (color == "зеленый" && isRoadClear) {
+  go = "подожди пока нарушители проедут";
+} else if (color == "желтый" && isRoadClear) {
+  go = "жди";
+} else if (color == "желтый" && !isRoadClear) {
+  go = "все рано жди";
+} else if (color == "красный" && !isRoadClear) {
+  go = "стой все рано";
+} else if (color == "красный" && isRoadClear) {
+  go = "стой и жди";
+} else {
+  go = "делай что хочешь!";
+}
+console.log(go);
